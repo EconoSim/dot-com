2387af82443a8eb1c056fd071e36f0db66c5afdc
josephbuhler <107206713+josephbuhler@users.noreply.github.com>
2024-12-30 21:09:25 -0600

    removed token correctly

---
+++ src/components/DevLog/DevLog.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './DevLog.css';
+
+const DevLog = ({ project, repository, branch, user, message, date }) => (
+    <div className="dev-log">
+        <div className="top-row">
+            <p className="project">{project}</p>
+            <p className="repository">{repository}</p>
+            <p className="branch">{branch}</p>
+        </div>
+        <div className="middle-row">
+            <p className="message">{message}</p>
+        </div>
+        <div className="bottom-row">
+            <p className="user">{user}</p>
+            <p className="date">{date}</p>
+        </div>
+    </div>
+);
+
+DevLog.propTypes = {
+    project: PropTypes.string.isRequired,
+    repository: PropTypes.string.isRequired,
+    branch: PropTypes.string.isRequired,
+    user: PropTypes.string.isRequired,
+    message: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+};
+
+export default DevLog;
